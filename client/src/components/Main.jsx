9c1265a7f9f3ec53a33e52815cf1b94b72ed5eda
Gyanendra Tiwari <mrgyan432@gmail.com>
2024-07-21 16:54:20 +0530

    layout changes

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -7,19 +7,19 @@ import CustomTabsDisplay from "./CustomTabsDisplay";
 
 export default function Main() {
   return (
-    <div className='p-5 rounded-md text-gray-950 flex flex-wrap gap-5 w-calc-100-minus-5vw'>
-      <div className="w-fit">
-        <Todo />
-        <Quote />
-        <Pomodoro />
-      </div>
+    <div className="p-5 rounded-md text-gray-950 flex flex-wrap gap-5 w-calc-100-minus-5vw">
       <div className="w-fit min-w-[600px]">
         <Announcements />
         <div className="flex items-center justify-between gap-3">
           <GoogleSlides />
         </div>
       </div>
+      <div className="w-fit">
+        <Todo />
+        <Pomodoro />
+        <Quote />
+      </div>
       <CustomTabsDisplay />
     </div>
-  )
+  );
 }
