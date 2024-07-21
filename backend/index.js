27289cc2fed6b634a15db10ff719d06b3fba1b68
MysteryHawk17 <raj.yash1217@gmail.com>
2024-07-21 14:13:00 +0530

    minor fix

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ wss.on("connection", (ws) => {
   ws.on("close", () => {
     console.log("Client disconnected");
   });
-  ws.send("Connected to socket server");
+  // ws.send("Connected to socket server");
 });
 
 const broadcastAnnouncement = (announcement) => {
